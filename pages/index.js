304dc34b94719350eaff243d93976129cb9499c1
prasanv <prasan.venkatb@gmail.com>
2024-05-08 23:03:10 -0700

    added all example pages

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,28 @@
-import { Link } from '@chakra-ui/react'
-import UseRefExample1 from '../components/UseRefExample1'
-import styles from '../styles/Home.module.css'
+import { Link } from "@chakra-ui/react";
+import UseRefExample1 from "../components/UseRefExample1";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div>
-        <Link href="/rhfexample1">Example 1</Link>
+        <ul>
+          <li>
+            <Link href="/rhfexample1">Example 1</Link>
+          </li>
+          <li>
+            <Link href="/rhfexample2">Example 2</Link>
+          </li>
+          <li>
+            <Link href="/rhfexample3">Example 3</Link>
+          </li>
+          <li>
+            <Link href="/rhfexample4">Example 4</Link>
+          </li>
+        </ul>
       </div>
       <p>React Hook Form </p>
       <UseRefExample1></UseRefExample1>
     </div>
-  )
+  );
 }
